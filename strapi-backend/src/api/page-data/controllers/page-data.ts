fab343bb569d0ea3211fe4ca155f51586e8193ec
Carlos Rugh <carlos.rugh@mmmake.com>
2024-01-18 16:35:53 +0000

    Merged PR 6558: feat: add pages and components in strapi

---
+++ strapi-backend/src/api/page-data/controllers/page-data.ts
@@ -0,0 +1,7 @@
+/**
+ * page-data controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::page-data.page-data');
---
+++ strapi-backend/src/api/page-data/routes/page-data.ts
@@ -0,0 +1,7 @@
+/**
+ * page-data router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::page-data.page-data');
