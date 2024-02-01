6af446730e987cb4d14a77d06080e1fcf4ee1266
Carlos Rugh <carlos.rugh@mmmake.com>
2024-02-01 18:04:55 +0000

    Merged PR 6665: fix: pipeline

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import { Environment } from './environment.interface';
 
 export const environment: Environment = {
   production: true,
-  strapiApiBaseUrl: '',
+  strapiApiBaseUrl: 'http://localhost:1337/api',
+  strapiBaseUrl: 'http://localhost:1337',
 };
